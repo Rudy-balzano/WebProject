bcf64cd8a204125d47a512b6a6e132344a78049b
rudy <rudybalzano16@gmail.com>
2020-03-06 17:43:41 +0100

    validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,12 @@
 class Admin < ApplicationRecord
 
     has_secure_password
+
+    validates :mail, format: {with: /\A[a-zA-Z]+$\z/}, uniqueness: {case_sensitive: false}
+
+    validates :prenom, format: {with: /\A[a-zA-Z]+$\z/}
+
+    validates :nom, format: {with: /\A[a-zA-Z]+$\z/}
+
     
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,20 @@ class Client < ApplicationRecord
 
     has_secure_password
 
-    
+    #validates :mail, format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/}, uniqueness: {case_sensitive: false}
+
+    validates :prenom, format: {with: /\A[a-zA-Z]+$\z/}
+
+    validates :nom, format: {with: /\A[a-zA-Z]+$\z/}
+
+    validates :rue, format: {with: /\A[a-zA-Z]+$\z/}
+
+    validates :ville, format: {with: /\A[a-zA-Z]+$\z/}
+
+    validates :numero, presence: true
+
+    validates :codePostal, presence: true
+
+
 
 end
